14a4e4033820d90e126f7645d9b352a617ed9dc9
William-Ogilvie <william.ogilvie@st-hughs.ox.ac.uk>
2026-03-17 18:28:29 +0000

    small grid search fewer layers

--- scripts/create_configs.py
+++ scripts/create_configs.py
@@ -16,9 +16,9 @@ BUBBLES = True
 TRUEVPRED = True
 METRICOEPOCH = True # metrics over epochs
 
-TIME_STAMP = "20260313_174800" 
+TIME_STAMP = "20260317_182500" 
 #"20260308_185500"
-TIME_STAMP_OOD = "20260313_175000" 
+TIME_STAMP_OOD = "20260317_182600" 
 #"20260308_185600"
 
 # - name: AEV-PLIG-Intra-Graphs-No-Scale-Rolling
@@ -247,17 +247,30 @@ def main():
     # - name: TAGCN-Drop-0-2-K-2-L-1-Dim-512-WD-0-30-1-Intra-OOD-Test
     # time_stamp: '20260311_081000'
     # Now try 2 layers but go K=2 to K=1
+    # benchmarks = ["OOD-Test"]
+    # graphs = ["inter", "intra"]
+    # models = ["TAGCN"]
+    # hidden_dim = [512] # also tried [512]
+    # layers = [2]
+    # K_values = [[2, 1]] # this means first layer K=2 second layer K=1
+    # heads_values = [1]
+    # dropout_values = [0.2]
+    # dropout_values_strings = ["0-2"]
+    # weight_decay_values = [0.0001]
+    # weight_decay_values_strings = ["0-30-1"]
+    # tried the above time_stamp: 20260313_174800
+    # Now going to perform a final grid search 
     benchmarks = ["OOD-Test"]
     graphs = ["inter", "intra"]
     models = ["TAGCN"]
-    hidden_dim = [512] # also tried [512]
+    hidden_dim = [256, 512, 768] # also tried [512]
     layers = [2]
-    K_values = [[2, 1]] # this means first layer K=2 second layer K=1
-    heads_values = [1]
-    dropout_values = [0.2]
-    dropout_values_strings = ["0-2"]
-    weight_decay_values = [0.0001]
-    weight_decay_values_strings = ["0-30-1"]
+    K_values = [1, 2, 3, [2, 1]] # this means first layer K=2 second layer K=1
+    heads_values = [1, 1, 1, 1]
+    dropout_values = [0.0, 0.2]
+    dropout_values_strings = ["0-0", "0-2"]
+    weight_decay_values = [0.0, 0.0001]
+    weight_decay_values_strings = ["0-0", "0-30-1"]
 
 
 
